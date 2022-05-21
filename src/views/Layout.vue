fe37d19ee6a25bb8f027c8776f9aa7df1b5d3841
coner <sptdfdzl@163.com>
2022-05-21 22:38:31 +0800

    feat: 首页初始化 样式混入 首页路由级别组件

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>布局容器</h1>
+  <nav>顶部通栏</nav>
+  <header>头部导航栏</header>
+  <!-- 主体区域 -->
+  <main>
+    <!-- 二级路由 -->
+    <router-view></router-view>
+  </main>
+  <footer>底部</footer>
 </template>
 
 <script>
 export default {
-  name: 'Layout',
+  name: 'xtx-layout',
   setup() {}
 }
 </script>
