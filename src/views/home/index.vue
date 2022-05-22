87e4e9004ae69c7bbef2f2aad95d05487a026021
coner <sptdfdzl@163.com>
2022-05-22 13:42:18 +0800

    feat: 完成首页轮播图功能

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,8 @@
       <div class="container">
         <!-- 左侧分类 -->
         <HomeCategory />
+        <!-- banner -->
+        <HomeBanner />
       </div>
     </div>
   </div>
@@ -11,10 +13,12 @@
 
 <script>
 import HomeCategory from './components/home-category.vue'
+import HomeBanner from './components/home-banner.vue'
 export default {
   name: 'xtx-home-page',
   components: {
-    HomeCategory
+    HomeCategory,
+    HomeBanner
   },
   setup() {}
 }
