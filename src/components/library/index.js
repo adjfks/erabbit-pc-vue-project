b6b8b77fd9817eb484a879a53fe5a02d19e3801b
coner <sptdfdzl@163.com>
2022-05-23 13:37:46 +0800

    feat: 面包屑组件 分类基础布局 批量注册组件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -2,11 +2,15 @@
 // 这就是插件
 // vue2.0插件写法要素：导出一个对象，有install函数，默认传入了Vue构造函数，Vue基础之上扩展
 // vue3.0插件写法要素：导出一个对象，有install函数，默认传入了app应用实例，app基础之上扩展
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
 // 默认显示图片
+// 使用webpack提供的require.context实现批量注册组件
+// 1. 路径 2. 是否加载子目录 3.匹配的正则表达式
 import defaultImg from '@/assets/images/200.png'
+const importFn = require.context('./', false, /\.vue$/)
 
 // 自定义指令：实现图片懒加载
 const defineDirective = (app) => {
@@ -33,9 +37,14 @@ export default {
   // 在app上进行扩展，app提供 component directive 函数
   // 如果要挂载原型 app.config.globalProperties 方式
   install(app) {
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBreadItem.name, XtxBreadItem)
+    importFn.keys().forEach((key) => {
+      const component = importFn(key).default
+      app.component(component.name, component)
+    })
     defineDirective(app)
   }
 }
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,12 +1,104 @@
 <template>
-  <div class="top-category">Top-Category</div>
+  <div class="top-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <XtxBread>
+        <XtxBreadItem to="/">首页</XtxBreadItem>
+        <XtxBreadItem to="/">{{ topCategory.name }}</XtxBreadItem>
+      </XtxBread>
+      <!-- 轮播图 -->
+      <XtxCarousel :sliders="sliders" style="height: 500px"></XtxCarousel>
+      <!-- 所有二级分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul v-if="topCategory.children && topCategory.children.length">
+          <li v-for="item in topCategory.children" :key="item.id">
+            <a href="javascript:;">
+              <img :src="item.picture" />
+              <p>{{ item.name }}</p>
+            </a>
+          </li>
+        </ul>
+        <!-- 骨架屏 -->
+        <ul v-else>
+          <li v-for="i in 7" :key="i">
+            <XtxSkeleton :width="'160px'" :height="'140px'"></XtxSkeleton>
+          </li>
+        </ul>
+      </div>
+      <!-- 不同分类商品 -->
+    </div>
+  </div>
 </template>
 
 <script>
+import { findBanner } from '@/api/home'
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+import XtxSkeleton from '@/components/library/xtx-skeleton.vue'
 export default {
   name: 'TopCategory',
-  setup() {}
+  setup() {
+    const store = useStore()
+    const route = useRoute()
+    // 轮播图
+    const sliders = ref([])
+    findBanner().then((data) => {
+      sliders.value = data.result
+    })
+    // 获取当前顶级分类
+    const topCategory = computed(() => {
+      let cate = {}
+      const item = store.state.category.list.find((item) => {
+        // 通过路由参数获取当前顶级分类的id
+        return item.id === route.params.id
+      })
+      if (item) cate = item
+      return cate
+    })
+    return { sliders, topCategory }
+  },
+  components: { XtxSkeleton }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+      li {
+        width: 168px;
+        height: 160px;
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+          img {
+            width: 100px;
+            height: 100px;
+          }
+          p {
+            line-height: 40px;
+          }
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
